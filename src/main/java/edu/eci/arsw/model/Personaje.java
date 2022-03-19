5d68f8d10da8f2e618082c2e90e9040a050971fc
Critian Castellanos <cristian.castellanos@mail.escuelaing.edu.co>
2022-03-18 22:33:11 -0500

    Model del juego iniciado

---
+++ src/main/java/edu/eci/arsw/model/Personaje.java
@@ -0,0 +1,26 @@
+package edu.eci.arsw.model;
+
+/**
+ * Clase que simula un personaje en el juego
+ */
+public abstract class Personaje{
+
+    private Tuple coordenadas;
+    private int vida;
+    private int daño;
+    public int VIDA = 100;
+    public int DAÑO = 10;
+
+    public Personaje(Tuple coordenada){
+        this.coordenadas = coordenada;
+    }
+
+    //Atacar
+
+    //Moverse
+
+
+
+
+
+}
